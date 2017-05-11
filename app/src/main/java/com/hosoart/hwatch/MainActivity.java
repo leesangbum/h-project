e0e41eb994185acd2c5f742f39e1450ec882d66c
hosup choi <mwmw77@gmail.com>
2017-05-12 00:32:04 +0900

    add login activity

--- app/src/main/java/com/hosoart/hwatch/MainActivity.java
+++ app/src/main/java/com/hosoart/hwatch/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hosoart.hwatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button btn =(Button)findViewById(R.id.button);
+		EditText edit = (EditText)findViewById(R.id.edit_text);
+
+		btn.setText("this is test");
 	}
 }
