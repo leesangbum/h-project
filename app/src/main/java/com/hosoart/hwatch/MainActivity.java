ef0924776cc463c923a8229e33ff9cbaabcd43c6
hosup choi <mwmw77@gmail.com>
2017-05-14 13:10:09 +0900

    add vibrator and activity change

--- app/src/main/java/com/hosoart/hwatch/MainActivity.java
+++ app/src/main/java/com/hosoart/hwatch/MainActivity.java
@@ -1,11 +1,19 @@
 package com.hosoart.hwatch;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+	static Vibrator vibrator;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
 		EditText edit = (EditText)findViewById(R.id.edit_text);
 
 		btn.setText("this is test");
+		Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+	}
+
+	public void onClick(View view) {
+		switch(view.getId()){
+			case R.id.button :
+				Toast.makeText(this, "this is test", Toast.LENGTH_SHORT).show();
+				vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+				vibrator.vibrate(100);
+				Intent intent = new Intent(this, LoginActivity.class);
+				startActivity(intent);
+				break;
+		}
 	}
 }
