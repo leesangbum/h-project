95902daac971a8a8481499b6cc3be8a7aba0d46c
hosup choi <mwmw77@gmail.com>
2017-05-23 00:43:26 +0900

    add handler for thread

--- app/src/main/java/com/hosoart/hwatch/LoginActivity.java
+++ app/src/main/java/com/hosoart/hwatch/LoginActivity.java
@@ -3,22 +3,22 @@ package com.hosoart.hwatch;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +28,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import junit.framework.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +46,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 	 * Id to identity READ_CONTACTS permission request.
 	 */
 	private static final int REQUEST_READ_CONTACTS = 0;
+	private static final String TAG = "hosup_login_activity";
+
 
 	/**
 	 * A dummy authentication store containing known user names and passwords.
@@ -66,8 +71,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		Intent intent = getIntent();
+		String data = intent.getStringExtra("value");
+		Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+		Log.v("hosup_login_activity",data);
+		Assert.assertEquals("Not hosup", data, "hosup");
+		setResult(1);
+
+
 		// Set up the login form.
 		mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+		mEmailView.setText(data);
 		populateAutoComplete();
 
 		mPasswordView = (EditText) findViewById(R.id.password);
--- app/src/main/java/com/hosoart/hwatch/MainActivity.java
+++ app/src/main/java/com/hosoart/hwatch/MainActivity.java
@@ -3,38 +3,121 @@ package com.hosoart.hwatch;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	static Vibrator vibrator;
+	static EditText edit;
+	private static final String TAG = "hosup_main_activity";
+	ExampleThread thread;
+	Handler mHandler = null;
+	Button btn;
+
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button btn =(Button)findViewById(R.id.button);
-		EditText edit = (EditText)findViewById(R.id.edit_text);
+		btn =(Button)findViewById(R.id.button);
+		edit = (EditText)findViewById(R.id.edit_text);
 
 		btn.setText("this is test");
 		Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+		Log.v(TAG,"MAIN Thread start");
+		//thread = new ExampleThread(0);
+		//thread.start();
+	//		for(int i = 0; i <=3 ; i++) {
+	//			thread = new ExampleThread(i);
+	//			thread.start();
+	//		}
+	//
+	//
+	//		try{
+	//			thread.join();
+	//		}catch (InterruptedException e){
+	//
+	//		}
+	//		Log.v(TAG,"MAIN Thread stop");
+
+		mHandler = new Handler();
+		Thread t = new Thread(new Runnable() {
+			int second = 0;
+			@Override
+			public void run() {
+				//ui x
+
+				for (int i = 0; i < 100; i++){
+					second++;
+				}
+				mHandler.postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						//ui o
+						btn.setText(Integer.toString(second));
+					}
+				},3000);
+			}
+		});
+		t.start();
+
 	}
 
 	public void onClick(View view) {
 		switch(view.getId()){
 			case R.id.button :
-				Toast.makeText(this, "this is test", Toast.LENGTH_SHORT).show();
 				vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
 				vibrator.vibrate(100);
+				String text = edit.getText().toString();
 				Intent intent = new Intent(this, LoginActivity.class);
-				startActivity(intent);
+				intent.putExtra("value", text);
+				startActivityForResult(intent,0);
 				break;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode){
+			case 0: //login
+				Log.v(TAG,String.valueOf(resultCode));
+				break;
+			default :
+				break;
+		}
+	}
+
+	private class ExampleThread extends Thread {
+		private static final String TAG_THREAD = "hosup_thread";
+		private int threadNum = 0;
+		public ExampleThread(int threadNum){
+			this.threadNum = threadNum;
+
+		}
+
+		@Override
+		public void run() {
+			super.run();
+			int second = 0;
+			//while(true){
+				second++;
+				try{
+					Log.v(TAG_THREAD, "start thread : ".concat(Integer.toString(threadNum)));
+					Thread.sleep(1000);
+				} catch (InterruptedException e){
+					e.printStackTrace();
+				}
+				Log.v(TAG_THREAD, "stop thread : ".concat(Integer.toString(threadNum)));
+			//}
 		}
 	}
 }
